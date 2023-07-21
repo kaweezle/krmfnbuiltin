752accae4cac5fcefb4a15b4f4e08c38b13b149f
Antoine Martin <antoine@openance.com>
2023-07-21 17:36:13 +0000

    🔖 Bump version to 0.4.3

--- main.go
+++ main.go
@@ -114,7 +114,7 @@ func main() {
 
 	cmd := command.Build(processor, command.StandaloneDisabled, false)
 	command.AddGenerateDockerfile(cmd)
-	cmd.Version = "v0.4.2" // <---VERSION--->
+	cmd.Version = "v0.4.3" // <---VERSION--->
 
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
